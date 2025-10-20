0fa469bdd510f3f61b595474b8828f6921a141f9
Shinto272000 <shintoshaju2018@gmail.com>
2025-10-20 12:10:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://amazonprojectdemo.netlify.app', 'http://localhost:5173'],
   credentials: true,
 }));
 
